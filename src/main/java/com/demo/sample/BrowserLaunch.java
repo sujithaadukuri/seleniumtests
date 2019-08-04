fd5be14dcb18531d0413f54a38896dc195274dba
sujitha <adhukurisujitha@gmail.com>
2019-08-05 03:43:59 +0530

    seleniumtest

---
+++ src/main/java/com/demo/sample/BrowserLaunch.java
@@ -0,0 +1,17 @@
+package com.demo.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BrowserLaunch {
+    WebDriver driver;
+    @Test
+    public void browserlaunch(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\IdeaProjects\\selenium2\\Drivers\\chromedriver_win32\\chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.get("https://www.flipkart.com");
+    }
+
+
+}
