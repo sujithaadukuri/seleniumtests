e6e47f228158393dd2abd8e66e4b5c921e70b486
sujitha <adhukurisujitha@gmail.com>
2019-08-06 12:34:46 +0530

    javaprogram

---
+++ src/main/java/com/java/programs/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.java.programs;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        boolean value =true;
+        value = false;
+        System.out.println("value of boolean variable:"+value);
+
+    }
+
+}
